6312db9bd5d489de095ac4c03237e13fc525ebcb
Omar Ali Sheikh <sheikhomar@gmail.com>
2017-11-25 11:39:31 +0100

    Solve challenge 2.

--- spark/HASHTAGS-s1962523-GRE.py
+++ spark/HASHTAGS-s1962523-GRE.py
@@ -1,11 +1,23 @@
-import MapReduce
-
 """
 Author: Omar Ali Sheikh-Omar (s1962523)
 """
 
 from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import re
 
 sc = SparkContext("local", "Hashtags")
 sc.setLogLevel("ERROR")
 
+def tokenise(row):
+    sanitised_text = row.text.lower()
+    return re.findall(r'#\w+', sanitised_text, flags=0)
+
+sqlc = SQLContext(sc)
+df = sqlc.read.json("/data/doina/twitterNL/201612/20161231-23.out.gz")
+rdd = df.select('text').rdd
+rdd = rdd.flatMap(tokenise)
+rdd = rdd.map(lambda hashtag: (hashtag, 1))
+rdd = rdd.reduceByKey(lambda acc, i: acc + i)
+rdd = rdd.sortBy(lambda tup: tup[1], ascending=False)
+print(rdd.take(20))
