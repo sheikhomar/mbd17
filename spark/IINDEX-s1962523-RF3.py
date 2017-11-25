c58806ba54a067ba970996975f81427536080b62
Omar Ali Sheikh <sheikhomar@gmail.com>
2017-11-25 08:19:43 +0100

    First attempt at challenge 1.

--- spark/IINDEX-s1962523-RF3.py
+++ spark/IINDEX-s1962523-RF3.py
@@ -9,3 +9,17 @@ from pyspark import SparkContext
 sc = SparkContext("local", "Inverted Index")
 sc.setLogLevel("ERROR")
 
+def to_word_file_tuple(filePath, fileContent):
+    words = fileContent.lower().split()
+    fileName = filePath[filePath.rfind('/')+1:]
+    return map(lambda word: (word, fileName), words)
+
+def pretty_print(list_of_tuples):
+    for tup in list_of_tuples:
+        print(u'Key: {} Value: {}'.format(tup[0], tup[1]))
+
+
+path = "/data/doina/Gutenberg-EBooks"
+rdd = sc.wholeTextFiles(path)
+rdd2 = rdd.flatMap(to_word_file_tuple)
+pretty_print(rdd2.take(20))
